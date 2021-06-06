8bd1a71fd064886239a48f3e6ee918eb96884f39
Luis Gutiérrez <luisghtz@outlook.com>
2021-06-06 12:51:33 -0500

    Create initialState and reducer

--- src/App.js
+++ src/App.js
@@ -1,4 +1,24 @@
+import { useEffect, useReducer } from 'react';
 import './App.css';
+
+const initialState = {
+  loading: true,
+  error: "",
+  todos: []
+}
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'SET_DATA': 
+      return {
+        loading: false,
+        error: "",
+        todos: action.payload
+      }
+    default:
+      break;
+  }
+}
 
 function App() {
   return (
